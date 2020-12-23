09bac941a914c7db8f9bc18e5451eed51d4a1da8
Tyler Fretz <fretztyler@gmail.com>
2020-12-22 22:20:31 -0500

    add front end build

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let phonebook = [
